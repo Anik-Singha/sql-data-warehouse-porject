3db5f98d256d64831b5f4e78fb3b0885c12f24e0
Anik Singha <93642306+Anik-Singha@users.noreply.github.com>
2026-02-03 22:25:09 +0530

    updating_quality_checks

---
+++ tests/quality_checks_gold.sql
@@ -0,0 +1,94 @@
+/*
+===============================================================================
+Quality Checks
+===============================================================================
+Script Purpose:
+    This script performs quality checks to validate the integrity, consistency, 
+    and accuracy of the Gold Layer. These checks ensure:
+    - Uniqueness of surrogate keys in dimension tables.
+    - Referential integrity between fact and dimension tables.
+    - Validation of relationships in the data model for analytical purposes.
+
+Usage Notes:
+    - Investigate and resolve any discrepancies found during the checks.
+===============================================================================
+*/
+
+-- ====================================================================
+-- Checking 'gold.dim_customers'
+-- ====================================================================
+-- Check for Uniqueness of Customer Key in gold.dim_customers
+-- Expectation: No results 
+
+-- Check if any duplicates were introduced by the Join Logic
+-- Expectation: No result
+SELECT cst_id, COUNT(*) 
+FROM (
+	SELECT 
+		ci.cst_id,
+		ci.cst_key,
+		ci.cst_firstname,
+		ci.cst_lastname,
+		ci.cst_marital_status,
+		ci.cst_gndr,
+		ci.cst_create_date,
+		ca.bdate,
+		ca.gen,
+		la.cid,
+		la.cntry
+	FROM silver.crm_cust_info AS ci
+	LEFT JOIN silver.erp_cust_az12 ca
+	ON ci.cst_key = ca.cid
+	LEFT JOIN silver.erp_loc_a101 la
+	ON ci.cst_key = la.cid
+) t 
+GROUP BY cst_id
+HAVING COUNT(*) > 1
+
+-----------------------------------------------
+-- We have 2 sources for the Gender Information, one from crm and other from erp
+-- We need to check for mismatch genders or NULLs introduced by JOIN logic
+SELECT DISTINCT
+
+	ci.cst_gndr,
+
+	ca.gen
+
+FROM silver.crm_cust_info AS ci
+LEFT JOIN silver.erp_cust_az12 ca
+ON ci.cst_key = ca.cid
+LEFT JOIN silver.erp_loc_a101 la
+ON ci.cst_key = la.cid
+ORDER BY 1,2
+
+-- silver.crm_prd_info 
+-- Checking Unique Product Key
+-- Expectation: No Result
+SELECT prd_key, COUNT(*) FROM (
+SELECT 
+	pn.prd_id, 
+	pn.prd_key,
+	pn.prd_nm,
+	pn.cat_id, 
+	pn.prd_cost, 
+	pn.prd_line, 
+	pn.prd_start_dt,
+	pc.cat,
+	pc.subcat,
+	pc.maintenance
+FROM silver.crm_prd_info pn
+LEFT JOIN silver.erp_px_cat_g1v2 pc
+ON pn.cat_id = pc.id
+WHERE prd_end_dt IS NULL	-- Filter out all historical data
+) t GROUP BY prd_key
+HAVING COUNT(*) > 1
+
+-- Foreign Key Integrity (Dimensions)
+-- Expectation: No result
+SELECT * 
+FROM gold.fact_sales s
+LEFT JOIN gold.dim_customers c
+ON c.customer_key = s.customer_key
+LEFT JOIN gold.dim_products p 
+ON p.product_key = s.product_key
+WHERE c.customer_key IS NULL
